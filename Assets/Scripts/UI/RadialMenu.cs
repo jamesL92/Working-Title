fc73f7f756e08ee42e0dcc8484eea8ba9a392cae
Lucas James <lucas.m.james92@gmail.com>
2018-11-17 13:24:48 +0000

    Added menu to the game for issuing actions.

---
+++ Assets/Scripts/UI/RadialMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialMenu : MonoBehaviour {
+
+	Button[] buttons;
+	public float distanceFromCenter;
+
+	void Start() {
+		buttons = GetComponentsInChildren<Button>();
+	}
+
+	void Update() {
+		int numButtons = buttons.Length;
+		float angleBetween = 360f / numButtons;
+
+		for(int i=0; i < numButtons; i++) {
+			buttons[i].transform.position = transform.position + new Vector3(
+				distanceFromCenter * Mathf.Cos(Mathf.Deg2Rad * -angleBetween * i),
+				distanceFromCenter * Mathf.Sin(Mathf.Deg2Rad * -angleBetween * i),
+				transform.position.z
+			);
+		}
+	}
+}
