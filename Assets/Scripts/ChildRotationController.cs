149f1f16d1176941a0023992340fcc2f600dcd21
Lucas James <lucas.m.james92@gmail.com>
2018-11-24 15:10:19 +0000

    Added utility monobehaviour to control rotation of its children

---
+++ Assets/Scripts/ChildRotationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ChildRotationController : MonoBehaviour {
+
+	[SerializeField] private Vector3 rotation;
+	// Update is called once per frame
+	void Update () {
+		foreach(Transform child in transform) {
+			child.rotation = Quaternion.Euler(rotation);
+		}
+	}
+}
