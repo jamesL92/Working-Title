92e03556ed3146f22a2d92927c2da6b9b15790ae
Lucas James <lucas.james@admiralgroup.co.uk>
2018-11-30 10:32:07 +0000

    Triggering method call when a unit would attack another unit.

--- Assets/Scripts/Card/UnitSlot.cs
+++ Assets/Scripts/Card/UnitSlot.cs
@@ -1,8 +1,30 @@
 using UnityEngine;
 
 public class UnitSlot: Zone {
+
+  public UnitSlot oppositeSlot;
+
+  private void Start() {
+    OnClick += UnitSlotClickHandler;
+  }
+
   public override bool AddCard(Card card) {
     card.Playable = false;
     return base.AddCard(card);
+  }
+
+  public Card GetUnit() {
+    if(Cards.Count > 0) {
+      return Cards[0];
+    }
+    return null;
+  }
+
+  private void UnitSlotClickHandler() {
+      Card myCard = GetUnit();
+      Card oppositeCard = oppositeSlot.GetUnit();
+      if(myCard && oppositeCard) {
+        Debug.Log(string.Format("Card is {0} is attacking card in slot {1}.", name, oppositeSlot.name));
+      }
   }
 }
